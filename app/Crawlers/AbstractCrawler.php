649b663ad1b3cb2c755bf7df20ea6c9d93b532b6
mojibakeo <mojibakeo@gmail.com>
2017-12-03 22:30:44 +0900

    init

---
+++ app/Crawlers/AbstractCrawler.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bko
+ * Date: 2017/12/03
+ * Time: 22:29
+ */
+namespace App\Crawlers;
+
+/**
+ * Class AbstractCrawler
+ * @package App\Crawlers
+ */
+abstract class AbstractCrawler
+{
+}
